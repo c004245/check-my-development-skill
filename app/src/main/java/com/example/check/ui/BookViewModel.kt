76cbea4f9195dcbe4a21708e5756af59052b114f
hyunwook <c004245@naver.com>
2021-06-24 00:29:49 +0900

    chore: hiltviewmodel inject

---
+++ app/src/main/java/com/example/check/ui/BookViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.check.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class BookViewModel @Inject constructor() : ViewModel() {
+
+}
--- app/src/main/java/com/example/check/ui/list/BookListFragment.kt
+++ app/src/main/java/com/example/check/ui/list/BookListFragment.kt
@@ -1,4 +1,26 @@
 package com.example.check.ui.list
 
-class BookListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.check.ui.BookViewModel
+
+
+class BookListFragment: Fragment() {
+
+    private val bookViewModel: BookViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = >ayotu
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
 }
